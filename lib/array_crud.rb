c9803f1038d9e72dad885d35b731587817bf82cc
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 01:53:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [1,2,3]
+  [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
   [1,2,3]
-  add_element_to_start_of_array << 4
+  array << 4
 end
 
 def add_element_to_start_of_array(array, element)
